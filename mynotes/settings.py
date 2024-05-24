d8072622bf1f0f8664aec5b872d330b97c54db22
Matheus Silva <matheus.souza28042001@gmail.com>
2024-05-24 12:41:51 -0300

    chore: update settings.py

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'frontend/build/static'
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
